5f5779374e33c79b5785bbffcd27b62e26cbd074
Rayane Souza <130245094+Rayane-Souza@users.noreply.github.com>
2024-10-07 14:37:20 -0300

    Adicionando estilização

--- my_project/produtos/urls.py
+++ my_project/produtos/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.produto_list, name='produto_list'),
     path('view/<int:pk>', views.produto_view, name='produto_view'),
-    path('new', views.produto_create, name='produto_new'),
+    path('new/', views.produto_create, name='produto_new'),
     path('detail/<int:pk>', views.produto_view, name='produto_detail'),
     path('edit/<int:pk>', views.produto_update, name='produto_edit'),
     path('delete/<int:pk>', views.produto_delete, name='produto_delete'),
