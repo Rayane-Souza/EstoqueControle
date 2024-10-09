492da6a3413e8cb3cfb9a92c4521a86a61f21be1
Luizh92 <138301226+Luizh92@users.noreply.github.com>
2024-10-09 20:19:58 -0300

    Update

--- my_project/my_project/urls.py
+++ my_project/my_project/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-     path('produtos/', include('produtos.urls')),
+     path('admin/', admin.site.urls),
      path('clientes/', include('clientes.urls')),
      path('pedidos/', include('pedidos.urls')),
      path('pagamentos/', include('pagamentos.urls')),
+     path('produtos/', include('produtos.urls')),
+     path('', lambda request: redirect('produto_list'))
 ]
